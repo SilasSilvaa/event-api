cf4c62c24601641f0eb3ecf2121d56be36b3798d
SilasSilvaa <silas-oli@hotmail.com>
2024-11-15 23:54:21 -0300

    refactor: add lombok annotations

--- api/src/main/java/com/ssilvadevevent/api/domain/address/Address.java
+++ api/src/main/java/com/ssilvadevevent/api/domain/address/Address.java
@@ -2,11 +2,19 @@ package com.ssilvadevevent.api.domain.address;
 
 import com.ssilvadevevent.api.domain.event.Event;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Table(name = "address")
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
     @Id
     @GeneratedValue
