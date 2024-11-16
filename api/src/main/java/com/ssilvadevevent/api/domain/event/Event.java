7c5f78d06bc985a8f056a4be72725eebb7033a2c
SilasSilvaa <silas-oli@hotmail.com>
2024-11-15 23:55:32 -0300

    feat: add table address mapping

--- api/src/main/java/com/ssilvadevevent/api/domain/event/Event.java
+++ api/src/main/java/com/ssilvadevevent/api/domain/event/Event.java
@@ -1,9 +1,7 @@
 package com.ssilvadevevent.api.domain.event;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.ssilvadevevent.api.domain.address.Address;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,7 +23,11 @@ public class Event {
 
     private String title;
     private String description;
+    private String imageUrl;
     private String eventUrl;
     private Boolean remote;
     private Date date;
+
+    @OneToOne(mappedBy = "event", cascade = CascadeType.ALL)
+    private Address address;
 }
