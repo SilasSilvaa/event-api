54e6eb59428c714be0936a7905cff2185532c348
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2025-02-23 15:46:42 -0300

    --

---
+++ src/test/java/com/ssilvadevevent/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssilvadevevent.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
